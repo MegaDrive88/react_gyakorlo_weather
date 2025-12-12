462760d31c423a094b14974b7156ad206ccac3d7
Podlipnik Ádám <adam.podlipnik@raabdigital.hu>
2025-12-12 14:54:14 +0100

    small fixes

--- src/components/DailyWeather/DailyWeatherCard/DailyWeatherCard.tsx
+++ src/components/DailyWeather/DailyWeatherCard/DailyWeatherCard.tsx
@@ -6,23 +6,26 @@ import type DailyWeahterCardData from '../../../interfaces/daily.weather.card.in
 
 export default function DailyWeatherCard(props: {data: DailyWeahterCardData, unit:string|undefined}){
     const [iconData, setIconData] = useState<{image:string}>()
+    const [dayName, setDayName] = useState<string>()
     const dataService = new DataService()
     useEffect(()=>{
         dataService.getWeatherIconSource(props.data.weather_code, 1).then(
             res => setIconData(res)
         )
-    }, [])
+        setDayName(new Date(props.data?.time).toLocaleDateString("hu-HU", {weekday:"long"})[0].toUpperCase() + 
+                   new Date(props.data?.time).toLocaleDateString("hu-HU", {weekday:"long"}).slice(1))
+    }, [props.data])
     return (
         <>
         <div className='dailyCard weatherBox lightBG'>
-            {new Date(props.data?.time).toLocaleDateString("hu-HU", {weekday:"long"})}
-            {/* egy nappal kesobbtol kene + kisebb font size szamokra*/}
+            {dayName} <br />
+            ({props.data?.time.replaceAll('-', '.')})
             <img src={iconData?.image} alt="" />
             <div className="row g-0">
-                <div className="col-6" style={{color: 'blue'}}>
+                <div className="col-lg-6" style={{color: 'blue', fontSize: "smaller"}}>
                     {props.data.temperature_2m_min} {props.unit}
                 </div>
-                <div className="col-6" style={{color: 'red'}}>
+                <div className="col-lg-6" style={{color: 'red', fontSize: "smaller"}}>
                     {props.data.temperature_2m_max} {props.unit}
                 </div>
             </div>
--- src/data.service.ts
+++ src/data.service.ts
@@ -12,7 +12,7 @@ export default class DataService{
         return (await fetch("https://gist.githubusercontent.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c/raw/76b0cb0ef0bfd8a2ec988aa54e30ecd1b483495d/descriptions.json").then(res=>res.json()))[wmo.toString()][is_day == 1 ? 'day' : 'night']
     }
     async getDailyForecastByLocation(lat: number, lon: number){
-        return (await this.axios.get<any>(`forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min,wind_direction_10m_dominant`)).data
+        return (await this.axios.get<any>(`forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min,wind_direction_10m_dominant&start_date=${new Date(Date.now() + 24 * 60 * 60 * 1000).toISOString().split('T')[0]}&end_date=${new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString().split('T')[0]}`)).data
         //&start_date=2025-12-13$end_date
     }
 }
